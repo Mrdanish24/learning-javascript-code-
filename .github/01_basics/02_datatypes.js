c8aa6430ee7af51c0c8927a129cda203ce80186c
Mrdanish24 <ansaridanish7080ansari@gmail.com>
2025-12-19 18:19:17 +0000

    data_type

---
+++ .github/01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+//alert("Danish") we are using node js .not brower
+
+console.log(3+3)
+
+let age = 18;
+let name = null;
+console.log(typeof age);
+
+console.log(typeof name); // object 
+
+// number => 2 to power 53
+// begint
+// string => ""
+// boolean => true/false
+// null => standaalone
+// undefine
+// symbol => unique
