9b21e0b0dfd42cd763ac709b712ded03f3a315d3
Mrdanish24 <ansaridanish7080ansari@gmail.com>
2025-12-26 17:52:02 +0000

    conversion of datatype

---
+++ .github/01_basics/03_conversionOperator.js
@@ -0,0 +1,17 @@
+//let score = "33danish"
+
+//console.log(typeof score)
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// 33abc => Nan
+
+let Number = 70
+
+let stringNumber = String(Number)
+console.log(stringNumber);
+
+console.log(typeof stringNumber)
