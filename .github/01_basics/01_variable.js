6631269e8b2cc4c961ac34c1789dc22b73acc900
Mrdanish24 <ansaridanish7080ansari@gmail.com>
2025-12-30 07:08:26 +0000

    03_variable

---
+++ .github/01_basics/01_variable.js
@@ -0,0 +1,6 @@
+
+const accountId = "4567"
+let accountEmail = "dhff@gamil.com"
+let accountPassword = "8906"
+let accountCity = "jaipur"
+console.table([accountEmail,accountId,accountPassword,accountCity]);
