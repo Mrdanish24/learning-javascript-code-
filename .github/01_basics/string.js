61e4a889078b3ddcb06dd2998822f960d55fee6b
Mrdanish24 <ansaridanish7080ansari@gmail.com>
2026-01-05 17:40:50 +0000

    string

---
+++ .github/01_basics/string.js
@@ -0,0 +1,19 @@
+const name = "Danish"
+const Age = 50
+
+// console.log(name + Age + " Done")
+
+console.log(`Hellow my Name is ${name} and my age is ${Age}`) // string interpolation are use 
+
+const userName = new  String("Danish01-hc-com")
+
+console.log(userName [0]);
+console.log(userName.__proto__);
+console.log(userName.length);
+console.log(userName.toUpperCase());
+console.log(userName.charAt(3));
+
+sp = userName.split("-");
+console.log(sp);
+
+
