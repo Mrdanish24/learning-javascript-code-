da8af7887bbb30bad15e30b7431f021611a24813
Mrdanish24 <ansaridanish7080ansari@gmail.com>
2026-01-07 16:55:13 +0000

    number and math

---
+++ .github/01_basics/num_and_math.js
@@ -0,0 +1,32 @@
+/*const age = 45;
+console.log(age);
+
+const count = new Number(100);
+console.log(count);
+
+console.log(count.toString().length);
+console.log(count.toFixed(2));
+
+const num = 23.5678
+
+console.log(num.toPrecision(4));
+
+const hundred = 100000000
+
+console.log(hundred.toLocaleString());
+*/
+//================== Math ====================
+
+
+/*console.log(Math);
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.floor(Math.random() * 10)+1);
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor(Math.random()*(max - min +1))+min);
+*/
+let dice = Math.floor(Math.random()*6)+1;
+console.log(dice);
