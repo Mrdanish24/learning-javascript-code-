2c81716a978cbba2f63bab1fea5b3d733c26f7df
Mrdanish24 <ansaridanish7080ansari@gmail.com>
2026-01-15 17:24:44 +0000

    date_time

---
+++ .github/01_basics/date_time.js
@@ -0,0 +1,15 @@
+// date 
+
+/*let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);*/
+
+let mytimeStamp = Date.now()
+console.log(mytimeStamp);
+
+newDate.tolocaleString('default',{
+    WeakSet:"long",
+})
