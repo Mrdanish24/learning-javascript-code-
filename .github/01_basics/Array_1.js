9eec81f134cf0c67f717d664126811a33526f4d0
Mrdanish24 <ansaridanish7080ansari@gmail.com>
2026-01-16 17:58:29 +0000

    Array

---
+++ .github/01_basics/Array_1.js
@@ -0,0 +1,47 @@
+//array
+//const myArr = [1,2,3,4,5,6];
+//const myArr1 = [7,10,9,11,45];
+
+
+// Array Method
+
+//myArr.push(10);
+//myArr.push(11);
+//myArr.pop(1)
+
+//myArr.unshift(9)
+//myArr.shift()
+
+
+//const  myn1 = myArr.slice(1,4)
+//console.log(myn1);
+//console.log(myArr);
+
+//const myn2 = myArr.splice(1,4)
+//console.log(myn2);
+//console.log(myArr);
+
+//console.log(myArr);
+//console.log(myArr1);
+
+//myArr.push(myArr1);
+//console.log(myArr);
+
+//const X = myArr.concat(myArr1);
+//console.log(X);
+
+//const A = [...myArr ,...myArr1];
+//console.log(A);
+
+//const A = [1,2,3,[2,3,4,[6,7,8],4],5];
+//console.log(A);
+//const B = A.flat(2);
+//console.log(B);
+
+Array.isArray("Danish");
+console.log(Array);
+Array.from("Danish")
+console.log(Array.from("Danish"));
+
+
+
